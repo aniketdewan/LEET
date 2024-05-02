4d0aca0bd6684be2c650ae72b2225c104a109918
aniketdewan <aniketsinghdewan513@gmail.com>
2024-05-02 17:33:13 +0530

    rotate

---
+++ rotatearray.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+import java.util.Arrays;
+
+public class rotatearray {
+    public static void main(String []args){
+        int nums[]={1,2,4,5};
+        int k=2;
+        System.out.println(Arrays.toString(rotate(nums,k)));
+    }
+    
+
+
+    public static int[]  rotate(int[] nums, int k) {
+        
+        int n=nums.length;
+        k%=nums.length;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+        return nums;
+       
+       
+    }
+    public static void reverse(int [] nums ,int start ,int end){
+        while(start<=end){
+
+        
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+         }
+          }
+}
